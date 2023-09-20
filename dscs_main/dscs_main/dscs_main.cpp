93354193bddf7e46757448e86a418141927f7e25
davidyang47 <yangjp1235@163.com>
2023-09-20 21:27:39 +0800

    deleted:    .vscode/c_cpp_properties.json 	modified:   .vscode/launch.json 	deleted:    .vscode/settings.json 	modified:   .vscode/tasks.json 	deleted:    dscs_main/.vs/dscs_main/FileContentIndex/0cb2e221-4aac-48ce-946e-76c2340bb379.vsidx 	new file:   dscs_main/.vs/dscs_main/FileContentIndex/72b14f8b-25a6-46ba-93e8-2577817ad162.vsidx 	modified:   dscs_main/.vs/dscs_main/v17/.suo 	modified:   dscs_main/.vs/dscs_main/v17/Browse.VC.db 	modified:   dscs_main/.vs/dscs_main/v17/ipch/AutoPCH/2c2dda9935f7ff53/DSCS_MAIN.ipch 	deleted:    dscs_main/.vscode/tasks.json 	modified:   dscs_main/dscs_main.sln 	modified:   dscs_main/dscs_main/dscs_main.cpp 	modified:   dscs_main/dscs_main/dscs_main.exe 	modified:   dscs_main/dscs_main/dscs_main.vcxproj 	new file:   dscs_main/dscs_main/test.csv 	new file:   dscs_main/dscs_main/x64/Debug/dscs_main.exe.recipe 	new file:   dscs_main/dscs_main/x64/Debug/dscs_main.ilk 	new file:   dscs_main/dscs_main/x64/Debug/dscs_main.log 	new file:   dscs_main/dscs_main/x64/Debug/dscs_main.obj 	new file:   dscs_main/dscs_main/x64/Debug/dscs_main.tlog/CL.command.1.tlog 	new file:   dscs_main/dscs_main/x64/Debug/dscs_main.tlog/CL.read.1.tlog 	new file:   dscs_main/dscs_main/x64/Debug/dscs_main.tlog/CL.write.1.tlog 	new file:   dscs_main/dscs_main/x64/Debug/dscs_main.tlog/Cl.items.tlog 	new file:   dscs_main/dscs_main/x64/Debug/dscs_main.tlog/dscs_main.lastbuildstate 	new file:   dscs_main/dscs_main/x64/Debug/dscs_main.tlog/link.command.1.tlog 	new file:   dscs_main/dscs_main/x64/Debug/dscs_main.tlog/link.read.1.tlog 	new file:   dscs_main/dscs_main/x64/Debug/dscs_main.tlog/link.write.1.tlog 	new file:   dscs_main/dscs_main/x64/Debug/vc143.idb 	new file:   dscs_main/dscs_main/x64/Debug/vc143.pdb 	deleted:    dscs_main/test.txt 	new file:   dscs_main/x64/Debug/dscs_main.exe 	new file:   dscs_main/x64/Debug/dscs_main.pdb

--- dscs_main/dscs_main/dscs_main.cpp
+++ dscs_main/dscs_main/dscs_main.cpp
@@ -1,16 +1,85 @@
-﻿#include<bits/stdc++.h>
+﻿#include <bits/stdc++.h>
 using namespace std;
+
+struct course{
+    int no;
+    string name;
+    int credits;
+    int hours;
+    vector<string> prerequisites;
+};
+vector<course> mycourses;
+
+bool write_in(){
+    ofstream csv_data("test.csv", ios::app);
+    if (!csv_data.is_open())
+    {
+        cout << "Error: opening file fail" << endl;
+        exit(1);
+    }
+    string line;
+    while(getline(cin, line)){
+        if(line=="end")
+            break;
+        else if(line=="")
+            continue;
+        else
+            csv_data<<line<<endl;
+    }
+    return true;
+}
+
+bool read_in(vector<course> &mycourses){
+    ifstream csv_data("test.csv", ios::in);
+    string line;
+
+    if (!csv_data.is_open())
+    {
+        cout << "Error: opening file fail" << endl;
+        exit(1);
+    }
+
+    istringstream sin;         //将整行字符串line读入到字符串istringstream中
+    vector<string> words; //声明一个字符串向量
+    string word;
+
+    // 读取标题行
+    getline(csv_data, line);
+    // 读取数据
+    while (getline(csv_data, line))
+    {
+        sin.clear();
+        sin.str(line);
+        words.clear();
+        while (getline(sin, word, ',')) //将字符串流sin中的字符读到word字符串中，以逗号为分隔符
+        {
+            words.push_back(word); //将每一格中的数据逐个push
+        }
+        course tmp;
+        tmp.no = stoi(words[0]);
+        tmp.name = words[1];
+        tmp.credits = stoi(words[2]);
+        tmp.hours = stoi(words[3]);
+        for (int i = 4; i < words.size(); i++)
+        {
+            tmp.prerequisites.push_back(words[i]);
+        }
+        mycourses.push_back(tmp);
+    }
+    csv_data.close();
+    return true;
+}
+
 int main()
 {
-    std::cout << "Hello World!\n";
-    cout<<"my name is dscs"<<endl;
-    int i=0;
-    int a;
-    cin>>a;
-    for(i=0;i<10;i++)
-    {
-        cout<<i<<endl;
+    write_in();
+    read_in(mycourses);
+    for(int i=0;i<mycourses.size();i++){
+        cout<<mycourses[i].no<<" "<<mycourses[i].name<<" "<<mycourses[i].credits<<" "<<mycourses[i].hours<<" ";
+        for(int j=0;j<mycourses[i].prerequisites.size();j++){
+            cout<<mycourses[i].prerequisites[j]<<" ";
+        }
+        cout<<endl;
     }
-    
-
+    return 0;
 }
